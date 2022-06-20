4e4d428e671236a84d0669a69b07da356d24181e
PedroConsuegra <pedroconsuegramateo@gmail.com>
2022-06-20 13:33:26 +0100

    Menu y Cambios de estilo

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MegaMenuItem,MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  public items: MegaMenuItem[] = [];
+  public popItems: MenuItem[] = [];
+
+
+  constructor() {
+   }
 
   ngOnInit(): void {
+    this.items = [
+      
+      {
+        label: 'Nombre Usuario', icon: 'pi pi-user'
+      },
+      {
+        label: 'Calendario', icon: 'pi pi-calendar'
+      },
+      {
+        label: 'Datos Respiratorios', icon: 'pi pi-sliders-h'
+      },
+      {
+        label: 'Actividad', icon: 'pi pi-heart-fill'
+      },
+      {
+        label: 'Resumen', icon: 'pi pi-book'
+      },
+      {
+        label: 'Ajustes', icon: 'pi pi-cog'
+      },
+    ];
+
+    this.popItems = [
+      {
+        label: 'Nombre usuario', icon: 'pi pi-user'
+      },
+      {
+        label: 'Calendario', icon: 'pi pi-calendar'
+      },
+      {
+        label: 'Datos Respiratorios', icon: 'pi pi-sliders-h'
+      },
+      {
+        label: 'Actividad', icon: 'pi pi-heart-fill'
+      },
+      {
+        label: 'Resumen', icon: 'pi pi-book'
+      },
+      {
+        label: 'Ajustes', icon: 'pi pi-cog'
+      },
+      {
+        label: 'Logout', icon: 'pi pi-sign-out'
+      },
+    ]
   }
+
 
 }
