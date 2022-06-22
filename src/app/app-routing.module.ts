cd915e9ae35a1db2f303b8e1e4de62211eb782e3
PedroConsuegra <pedroconsuegramateo@gmail.com>
2022-06-22 08:57:34 +0100

    estres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CalendarioComponent } from './contenido/components/calendario/calendario.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
