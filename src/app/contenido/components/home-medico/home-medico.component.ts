27ca127f67385d69dfe5fe6293ca3084134be04b
PedroConsuegra <pedroconsuegramateo@gmail.com>
2022-06-27 09:19:08 +0100

    -

--- src/app/contenido/components/home-medico/home-medico.component.ts
+++ src/app/contenido/components/home-medico/home-medico.component.ts
@@ -9,7 +9,61 @@ import { Paciente } from 'src/app/shared/interfaces/paciente.interface';
 export class HomeMedicoComponent implements OnInit {
   public pacientes: Paciente[];
   constructor() {
-    this.pacientes = [];
+    this.pacientes = [{
+      dni: '01938429N',
+      nombre: 'Pedro Consuegra',
+      email: 'pedroconsuegra@paciente.com',
+      genero: 'm',
+      peso: 63,
+      altura: 1.69,
+      mutacion1: 'F508D',
+      mutacion2: 'G542X',
+      idUsuario: 'pedroConsuegra'
+    },
+    {
+      dni: '01938429N',
+      nombre: 'Pedro Consuegra',
+      email: 'pedroconsuegra@paciente.com',
+      genero: 'm',
+      peso: 63,
+      altura: 1.69,
+      mutacion1: 'F508D',
+      mutacion2: 'G542X',
+      idUsuario: 'pedroConsuegra'
+    },
+    {
+      dni: '01938429N',
+      nombre: 'Pedro Consuegra',
+      email: 'pedroconsuegra@paciente.com',
+      genero: 'm',
+      peso: 63,
+      altura: 1.69,
+      mutacion1: 'F508D',
+      mutacion2: 'G542X',
+      idUsuario: 'pedroConsuegra'
+    },
+    {
+      dni: '01938429N',
+      nombre: 'Pedro Consuegra',
+      email: 'pedroconsuegra@paciente.com',
+      genero: 'm',
+      peso: 63,
+      altura: 1.69,
+      mutacion1: 'F508D',
+      mutacion2: 'G542X',
+      idUsuario: 'pedroConsuegra'
+    },
+    {
+      dni: '01938429N',
+      nombre: 'Pedro Consuegra',
+      email: 'pedroconsuegra@paciente.com',
+      genero: 'm',
+      peso: 63,
+      altura: 1.69,
+      mutacion1: 'F508D',
+      mutacion2: 'G542X',
+      idUsuario: 'pedroConsuegra'
+    }];
    }
 
   ngOnInit(): void {
